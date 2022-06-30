7aedcb11411dec7b1bbe64b83e552d66c2cd6de5
AlexandrTarasov <torasov.alexandr@gmail.com>
2022-06-30 10:09:24 +0300

    code style fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,13 @@ module.exports = {
     '@typescript-eslint',
   ],
   'rules': {
+    'linebreak-style': 0,
+    'react/prop-types': 0,
+    'require-jsdoc': 0,
+    'max-len': [1, {'code': 100}],
+    'react/display-name': 0,
+    'react/no-unescaped-entities': 0,
+    'no-unused-vars': 1,
+    'camelcase': 'off',
   },
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import {Provider} from "react-redux";
-import store from "../store/store";
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import {Provider} from 'react-redux';
+import store from '../store/store';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   return (<Provider store={store}>
   return <Component {...pageProps} />
-  </Provider>)
+  </Provider>);
 }
 
-export default MyApp
+export default MyApp;
