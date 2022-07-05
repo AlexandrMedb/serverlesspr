9d14fadd663e10580566ca7662022c41bd1744e7
Alexandr <torasov.alexandr@gmail.com>
2022-07-05 17:47:24 +0300

    main page style fixing

--- src/containers/mainHeader.tsx
+++ src/containers/mainHeader.tsx
@@ -3,11 +3,15 @@ import {LinkMain} from '../components/linkMain';
 import {useRouter} from 'next/router';
 import {ButtonC} from '../components/buttonC';
 import Link from 'next/link';
+import {useState} from 'react';
 
 
 export const MainHeader=()=>{
   const router = useRouter();
-  return (
+
+  const [menuOpen, setMenuOpen]=useState(false);
+
+  return (<>
     <header className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.left}>
@@ -18,12 +22,21 @@ export const MainHeader=()=>{
         <div className={styles.right}>
           <LinkMain href={'/project'} text={'Main'}/>
           <LinkMain href={'/login/login'} text={'login'} setSecondColor={true}/>
-
           <ButtonC text={'Sing Up'} onClick={()=>router.push('/login/singUp')}/>
-
         </div>
+        <div className={styles.burgerMenuIcon}><img onClick={()=>setMenuOpen(!menuOpen)}
+          src="/icons/bars-solid.svg" alt=""/></div>
+
 
       </div>
+
     </header>
+    {menuOpen &&
+            <div className={styles.burgerMenu}>
+              <ButtonC text={'Sing Up'} onClick={()=>router.push('/login/singUp')}/>
+              <ButtonC text={'Login'} onClick={()=>router.push('/login/login')}/>
+              <ButtonC text={'Main'} onClick={()=>router.push('/project')}/>
+            </div>}
+  </>
   );
 };
